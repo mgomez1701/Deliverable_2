bb0b7bbc1a86b9efe5c95477ab83d5b7e35158d8
Mike <michael005@me.com>
2019-09-13 18:23:08 -0400

    Set up the first 4 choices

--- Deliverable2/Deliverable2/Deliverable2.cs
+++ Deliverable2/Deliverable2/Deliverable2.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace Deliverable2
 {
-    class Program
+    class Deliverable2
     {
         static void Main(string[] args)
         {
+
+            string optionOne, optionTwo, optionThree, optionFour, a, b, c, d;
+            
+
+            Console.WriteLine("Hello user, what are you in the mood for?");
+            Console.WriteLine("Here are your options: ");
+            
+          
+
+            Console.WriteLine(a = "1). Action");
+            Console.WriteLine(b = "2). Chill Out");
+            Console.WriteLine(c = "3). Push it to the limit");
+            Console.WriteLine(d = "1). Craving food");
+            Console.ReadLine();
+
+
+
+
+
+
+
         }
+
+
+
+
     }
+         
 }
