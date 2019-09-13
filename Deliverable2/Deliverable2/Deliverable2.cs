1870b6a828f514066486cc5319a630db0b55ad06
Mike <michael005@me.com>
2019-09-13 18:57:00 -0400

    First If Statement functional

--- Deliverable2/Deliverable2/Deliverable2.cs
+++ Deliverable2/Deliverable2/Deliverable2.cs
@@ -11,30 +11,32 @@ namespace Deliverable2
         static void Main(string[] args)
         {
 
-            string optionOne, optionTwo, optionThree, optionFour, a, b, c, d;
+            string a, b, c, d;
             
 
             Console.WriteLine("Hello user, what are you in the mood for?");
             Console.WriteLine("Here are your options: ");
-            
-          
-
-            Console.WriteLine(a = "1). Action");
-            optionOne = Console.ReadLine();
-
-            Console.WriteLine(b = "2). Chill Out");
-            optionTwo = Console.ReadLine();
-
-            Console.WriteLine(c = "3). Push it to the limit");
-            optionThree = Console.ReadLine();
-
-            Console.WriteLine(d = "4). Craving food");
-            optionFour = Console.ReadLine();
+                 
+            Console.WriteLine(a = "1). Action");         
+            Console.WriteLine(b = "2). Chill Out");         
+            Console.WriteLine(c = "3). Push it to the limit");           
+            Console.WriteLine(d = "4). Craving food");         
             Console.ReadLine();
 
-
-
-
+            if (a == "1" || a == "Action" || a == "ACTION" ||
+                b == "2" || b == "Chill Out" || b == "CHILL OUT" ||
+                c == "3" || c == "Push it to the limit" || c == "PUSH IT TO THE LIMIT" ||
+                d == "4" || d == "Craving food" || b == "CRAVING FOOD")
+            {
+                Console.WriteLine("I didn't get that. Try one more time. You got this!");
+                Console.ReadLine();
+               
+            }
+            else
+            {
+                Console.WriteLine("How many people are you going to bring?");
+                Console.ReadLine();
+            }
 
 
 
