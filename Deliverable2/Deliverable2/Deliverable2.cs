6e7e1a5a27daf8c9884a4fcbfdfd405a34349de1
Mike <michael005@me.com>
2019-09-15 12:48:37 -0400

    Working on bool , maybe looking at swithes

--- Deliverable2/Deliverable2/Deliverable2.cs
+++ Deliverable2/Deliverable2/Deliverable2.cs
@@ -11,7 +11,9 @@ namespace Deliverable2
         static void Main(string[] args)
         {
 
-            string question, question2; 
+            string question ="";
+            bool Answer1 = true, Answer2 = true, Answer3 =true, Answer4 =true;
+            int number1 = 0 , number2 = 4, number3 = 10, number4 = 11;
 
 
             Console.WriteLine("Hello user, what are you in the mood for?");
@@ -22,17 +24,18 @@ namespace Deliverable2
             Console.WriteLine("Chill Out");
             Console.WriteLine("Danger");
             Console.WriteLine("Craving food");
-            question = Convert.ToString(Console.ReadLine());
+            Console.ReadLine();
             
+            
+           
 
-
-            if (question == "Action" || question == "Chill Out" || question == "Danger" || question == "Craving Food")
+            if (question != "Action" || question != "Chill Out" || question != "Danger" || question != "Craving Food")
             {
-                Console.WriteLine("How many people are you bringing with you?");
-                Console.WriteLine("0");
-                Console.WriteLine("1-4?");
-                Console.WriteLine("5-10");
-                Console.WriteLine("11+?");
+                Console.WriteLine("How many people are you bringing with you?");        
+                Console.WriteLine("0",number1);
+                Console.WriteLine("1-4?",number2);
+                Console.WriteLine("5-10",number3);
+                Console.WriteLine("11+?",number4);
                 Console.ReadLine();
 
             }
@@ -41,9 +44,47 @@ namespace Deliverable2
             {
                 Console.WriteLine("I did not understand. Please try again.");
                 Console.ReadLine();
+                return;
 
                 
             }
+             
+                if (Answer1 != true || Answer2 != true || Answer3 != true || Answer4 != true)
+            {
+                Console.WriteLine("You should go to Taco Bell in a Sedan");
+                Console.WriteLine("Thanks for chatting with me! Goodbye!");
+                Console.ReadLine();
+                }
+
+                else if (Answer2 != true || Answer1 !=true || Answer3 != true || Answer4 !=true)
+                {
+                Console.WriteLine(" You should go Hiking up the mountain. Best views out there. I would pack your best sneakers.");
+                Console.WriteLine("Thanks for chatting with me! Goodbye!");
+                Console.ReadLine();
+                }
+
+                else if (Answer3 != true || Answer1 != true || Answer2 != true || Answer4 != true)
+                {
+                Console.WriteLine("I laugh in the face of Danger! -Simba Voice. I can lend you an Airplane to go Skydiving in. You should do that.");
+                Console.WriteLine("Thanks for chatting with me! Goodbye!");
+                Console.ReadLine();
+                }
+
+                else if (Answer4 == true || Answer1 != true || Answer2 != true || Answer4 != true)
+                {
+                Console.WriteLine("The best thing to do would be go Stock Car Racing in a Volkswagon Bus");
+                Console.WriteLine("Thanks for chatting with me! Goodbye!");
+                Console.ReadLine();
+                }
+
+                else
+                {
+                Console.WriteLine("I did not understand. Please try again.");
+                Console.ReadLine();
+                return;
+                }
+
+
 
                 
         }
