5675920c4083d5aa81bb4db773bd27e501761c56
Mike <michael005@me.com>
2019-09-15 21:32:59 -0400

    Doing switches instead for efficiency

--- Deliverable2/Deliverable2/Deliverable2.cs
+++ Deliverable2/Deliverable2/Deliverable2.cs
@@ -11,81 +11,40 @@ namespace Deliverable2
         static void Main(string[] args)
         {
 
-            string question ="";
-            bool Answer1 = true, Answer2 = true, Answer3 =true, Answer4 =true;
-            int number1 = 0 , number2 = 4, number3 = 10, number4 = 11;
+            string ch;
 
 
             Console.WriteLine("Hello user, what are you in the mood for?");
             Console.WriteLine("Here are your options: ");
             
-
             Console.WriteLine("Action");
             Console.WriteLine("Chill Out");
             Console.WriteLine("Danger");
             Console.WriteLine("Craving food");
-            Console.ReadLine();
-            
+            ch = Convert.ToString(Console.ReadLine()); 
             
            
-
-            if (question != "Action" || question != "Chill Out" || question != "Danger" || question != "Craving Food")
+            switch (ch)
             {
-                Console.WriteLine("How many people are you bringing with you?");        
-                Console.WriteLine("0",number1);
-                Console.WriteLine("1-4?",number2);
-                Console.WriteLine("5-10",number3);
-                Console.WriteLine("11+?",number4);
-                Console.ReadLine();
-
+                case "Action":
+                    Console.WriteLine("How many of you are going?");
+                    Console.ReadLine();
+                    break;
+                case "Chill Out":
+                    Console.WriteLine("How many of you are going?");
+                    Console.ReadLine();
+                    break;
+                case "Danger":
+                    Console.WriteLine("How many of you are going?");
+                    Console.ReadLine();
+                    break;
+                case "Craving Food":
+                    Console.WriteLine("How many of you are going?");
+                    Console.ReadLine();
+                    break;
             }
 
-            else 
-            {
-                Console.WriteLine("I did not understand. Please try again.");
-                Console.ReadLine();
-                return;
-
-                
-            }
-             
-                if (Answer1 != true || Answer2 != true || Answer3 != true || Answer4 != true)
-            {
-                Console.WriteLine("You should go to Taco Bell in a Sedan");
-                Console.WriteLine("Thanks for chatting with me! Goodbye!");
-                Console.ReadLine();
-                }
-
-                else if (Answer2 != true || Answer1 !=true || Answer3 != true || Answer4 !=true)
-                {
-                Console.WriteLine(" You should go Hiking up the mountain. Best views out there. I would pack your best sneakers.");
-                Console.WriteLine("Thanks for chatting with me! Goodbye!");
-                Console.ReadLine();
-                }
-
-                else if (Answer3 != true || Answer1 != true || Answer2 != true || Answer4 != true)
-                {
-                Console.WriteLine("I laugh in the face of Danger! -Simba Voice. I can lend you an Airplane to go Skydiving in. You should do that.");
-                Console.WriteLine("Thanks for chatting with me! Goodbye!");
-                Console.ReadLine();
-                }
-
-                else if (Answer4 == true || Answer1 != true || Answer2 != true || Answer4 != true)
-                {
-                Console.WriteLine("The best thing to do would be go Stock Car Racing in a Volkswagon Bus");
-                Console.WriteLine("Thanks for chatting with me! Goodbye!");
-                Console.ReadLine();
-                }
-
-                else
-                {
-                Console.WriteLine("I did not understand. Please try again.");
-                Console.ReadLine();
-                return;
-                }
-
-
-
+            Console.ReadLine();
                 
         }
     }
