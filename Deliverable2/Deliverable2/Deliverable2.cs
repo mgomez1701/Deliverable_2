036471faf526fa7c6d48b6ad3279608142aed140
Mike <michael005@me.com>
2019-09-22 03:47:44 -0400

    Stored the moods and added If Else Statement

--- Deliverable2/Deliverable2/Deliverable2.cs
+++ Deliverable2/Deliverable2/Deliverable2.cs
@@ -11,81 +11,93 @@ namespace Deliverable2
         static void Main(string[] args)
         {
 
-            string ch, rh;
+            string ch;
+            int value= 0;
+
+
 
 
             Console.WriteLine("Hello user, what are you in the mood for?");
             Console.WriteLine("Here are your options: ");
-            
+
             Console.WriteLine("Action");
             Console.WriteLine("Chill Out");
             Console.WriteLine("Danger");
             Console.WriteLine("Craving food");
             ch = Convert.ToString(Console.ReadLine());
-          
+
 
             switch (ch)
-            {
+            { 
                 case "Action":
+                    Console.WriteLine("I love doing action packed activities! How many of you are going?");
+                    
+                    value = int.Parse(Console.ReadLine());
 
-                    Console.WriteLine("How many of you are going?");
-                 
                     break;
+
                 case "Chill Out":
-                    Console.WriteLine("How many of you are going?");
-                   
-                    break;
-                case "Danger":
-                    Console.WriteLine("How many of you are going?");
-                  
-                    break;
-                case "Craving Food":
-                    Console.WriteLine("How many of you are going?");                
-                   
+                    Console.WriteLine("Chilling out is what I do best. How many of you are going?");
+                    value = int.Parse(Console.ReadLine());
 
                     break;
+
+                case "Danger":
+                    Console.WriteLine("You are freaking me out, but I will help. How many of you are going?");
+                    value = int.Parse(Console.ReadLine());
+
+                    break;
+
+                case "Craving Food":
+                    Console.WriteLine("I got the best spot for you to How many of you are going?");
+                    value = int.Parse(Console.ReadLine());
+
+                    break;
+
                 default:
                     Console.WriteLine("I am not yet programmed to understand that. Please make sure to type the words out exactly how they look. Caps matter.");
                     Console.ReadLine();
+
                     break;
 
             }
-                   
-            Console.WriteLine("0");
-            Console.WriteLine("1-4");
-            Console.WriteLine("5-10");
-            Console.WriteLine("11+");
-            rh = Convert.ToString(Console.ReadLine());
 
-            switch (rh)
+
+
+            
+
+            if (value == 0 || value <= 11  && ch == "Craving Food")
             {
-                case "0":
-                    Console.WriteLine("If you are by yourself you should go to Taco Bell in a Sedan?");
-                    Console.WriteLine("Thank you for chatting with me. Have fun. Good-bye!");
-                    Console.ReadLine();
-                    break;
-                case "1-4":
-                    Console.WriteLine("You all shouuld go Hiking in the mountains. Make sure you pack your sneakers!");
-                    Console.WriteLine("Thank you for chatting with me. Have fun. Good-bye!");
-                    Console.ReadLine();
-                    break;
-                case "5-10":
-                    Console.WriteLine("I think going Stock Car Racing in a Volkswagon Bus would be a sight to see.");
-                    Console.WriteLine("Thank you for chatting with me. Have fun. Good-bye!");
-                    Console.ReadLine();
-                    break;
-                case "11+":
-                    Console.WriteLine("All of you should go Sky Diving! I have an Airplane that you can borrow!");
-                    Console.WriteLine("Thank you for chatting with me. Have fun. Good-bye!");
-                    Console.ReadLine();
-                    break;
-                default:
-                    Console.WriteLine("I am not yet programmed to understand that. Please make sure you type the numbers out exactly like they look.");
-                    Console.ReadLine();
-                    break;
-
-                    
+                Console.WriteLine("Okay if you are in the mood for good food, I would go to Taco Bell in a Sedan");
+                Console.WriteLine("Thank you for chatting with me. Have fun. Good-bye!");
+                Console.ReadLine();
             }
+
+            else if (value != 0 || value <= 4 && ch == "Chill Out")
+            {
+                Console.WriteLine("If you are looking to chill out, you should go Hiking in the mountain. Be sure to pack your Sneakers");
+                Console.WriteLine("Thank you for chatting with me. Have fun. Good-bye!");
+                Console.ReadLine();           
+            }
+            
+            else if (value != 0 || value <= 10 || value >= 5 || ch == "Action")
+            {
+                Console.WriteLine("If you are looking for action, Stock Car Racing in a Volkswagon Bus would be perfect!");
+                Console.WriteLine("Thank you for chatting with me. Have fun. Good-bye!");
+                Console.ReadLine();
+            }
+
+            else if (value != 0 || value >= 11 || value <= 10  && ch == "Danger")
+            {
+                Console.WriteLine("If you are looking for danger, you should go Sky Diving! I have an Airplane that you can borrow!");
+                Console.WriteLine("Thank you for chatting with me. Have fun. Good-bye!");
+                Console.ReadLine();
+            }
+
+            else
+
+            Console.WriteLine("I am not yet programmed to understand that. Please make sure you type the numbers out exactly like they look.");
+            Console.ReadLine();
 
             Console.ReadLine();
                 
